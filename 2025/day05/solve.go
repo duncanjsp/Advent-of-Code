1c4f3f33c63b402b19d70f6eff3edf809d8bb344
Duncan Stevenson-Price <duncan@psmail.co>
2025-12-05 14:21:33 +0000

    Day 5 scaffold

---
+++ 2025/day05/solve.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"strings"
+)
+
+func Solve(input string) (int, int) {
+	lines := strings.Split(strings.TrimSpace(input), "\n")
+
+	part1 := 0
+	part2 := 0
+
+	_ = lines // remove this once used
+	return part1, part2
+}
